b72f813d17ecdd24fe3fbd8ab33896aa80318646
rgsk <guptarahul70322@gmail.com>
2023-08-20 13:34:46 +0530

    more probs

--- src/lib/ListNode.py
+++ src/lib/ListNode.py
@@ -37,3 +37,46 @@ def getLength(head: Optional[ListNode]) -> int:
         temp = temp.next
         count += 1
     return count
+
+
+def at(head: Optional[ListNode], idx: int):
+    length = getLength(head)
+    if idx < 0:
+        idx = length + idx
+    if idx < 0 or idx >= length:
+        raise Exception("index out of range")
+    temp = head
+    while idx > 0:
+        if temp is not None:
+            temp = temp.next
+        else:
+            raise Exception("index out of range")
+        idx -= 1
+    return temp
+
+
+def equal(first: Optional[ListNode], second: Optional[ListNode]):
+    ft = first
+    st = second
+
+    while ft is not None and st is not None:
+        if ft.val != st.val:
+            return False
+        ft = ft.next
+        st = st.next
+
+    if ft is None and st is None:
+        return True
+    return False
+
+
+def reverseList(head: Optional[ListNode]) -> Optional[ListNode]:
+    prev = None
+    cur = head
+    next = head
+    while cur:
+        next = cur.next
+        cur.next = prev
+        prev = cur
+        cur = next
+    return prev
