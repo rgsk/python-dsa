5052f663d807cadbb4f549218ae62af510181eb1
rgsk <guptarahul70322@gmail.com>
2023-09-05 18:53:03 +0530

    boundary traversal

--- src/lib/TreeNode.py
+++ src/lib/TreeNode.py
@@ -28,8 +28,65 @@ class TreeNode:
     def __repr__(self) -> str:
         return str(getList(self))
 
+    def display(self):
+        lines, *_ = self._display_aux()
+        for line in lines:
+            print(line)
 
-def getHeight(root: TreeNode):
+    def _display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if self.right is None and self.left is None:
+            line = '%s' % self.val
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if self.right is None:
+            lines, n, p, x = self.left._display_aux()
+            s = '%s' % self.val
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if self.left is None:
+            lines, n, p, x = self.right._display_aux()
+            s = '%s' % self.val
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left._display_aux()
+        right, m, q, y = self.right._display_aux()
+        s = '%s' % self.val
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * \
+            '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + \
+            (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + \
+            [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+class BinaryTreeNode(TreeNode):
+    pass
+
+
+def getHeight(root: Optional[TreeNode]):
     if root is None:
         return 0
     else:
@@ -159,4 +216,42 @@ def levelOrder3(root: Optional[TreeNode]) -> List[List[int]]:
             row = []
             if queue:
                 queue.append(None)
+    return ans
+
+
+def levelOrderNodes(root: Optional[TreeNode]) -> List[List[TreeNode]]:
+    ans: List[List[TreeNode]] = []
+    queue = deque([root, None])
+    row: List[TreeNode] = []
+    while queue:
+        last = queue.popleft()
+        if last:
+            row.append(last)
+            if last.left:
+                queue.append(last.left)
+            if last.right:
+                queue.append(last.right)
+        else:
+            ans.append(row)
+            row = []
+            if queue:
+                queue.append(None)
+    return ans
+
+
+def isLeaf(node: Optional[TreeNode]):
+    return node and node.left is None and node.right is None
+
+
+def getLeafs(root: Optional[TreeNode]):
+    ans: List[TreeNode] = []
+
+    def helper(node: Optional[TreeNode]):
+        if node:
+            if isLeaf(node):
+                ans.append(node)
+            else:
+                helper(node.left)
+                helper(node.right)
+    helper(root)
     return ans
