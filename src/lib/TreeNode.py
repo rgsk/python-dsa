21e58bf819fda64c8fcd35d1615cd4378c131fda
rgsk <guptarahul70322@gmail.com>
2023-12-24 15:25:21 +0530

    BSTIterator

--- src/lib/TreeNode.py
+++ src/lib/TreeNode.py
@@ -325,3 +325,25 @@ def getPostorderTraversal(root: TreeNode):
             values.append(root.val)
     postorder(root)
     return values
+
+
+class BSTIterator:
+    # @param root, a binary search tree's root node
+    def __init__(self, root):
+        self.stack = []
+        while root:
+            self.stack.append(root)
+            root = root.left
+
+    # @return a boolean, whether we have a next smallest number
+    def hasNext(self):
+        return len(self.stack) > 0
+
+    # @return an integer, the next smallest number
+    def next(self):
+        x = self.stack.pop()
+        temp = x.right
+        while temp:
+            self.stack.append(temp)
+            temp = temp.left
+        return x.val
