d8e49bdb53a7f17b4227f982b996d9a38531a6e5
rgsk <guptarahul70322@gmail.com>
2024-08-23 12:03:44 +0530

    solved

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
+# export ONLINE_JUDGE=true
+
 import os
 import sys
+from math import log2
 
 if os.getenv("ONLINE_JUDGE") is not None:
     script_directory = os.path.dirname(os.path.realpath(__file__))
@@ -11,36 +14,25 @@ if os.getenv("ONLINE_JUDGE") is not None:
     output_file_path = os.path.join(script_directory, 'output.txt')
     sys.stdout = open(output_file_path, 'w')
 
-MOD = 1000000007
-
 
 def main():
-    n = int(input())
-    total = (n * (n + 1)) // 2
-
-    if total % 2 == 0:
-        halfSum = total // 2
-        dp = [0] * (halfSum + 1)
-
-        for value in range(n + 1, 0, -1):
-            prev = dp[:]
-            for remaining in range(halfSum + 1):
-                if value == n + 1:
-                    if remaining == 0:
-                        dp[remaining] = 1
-                    else:
-                        dp[remaining] = 0
-                else:
-                    if remaining >= value:
-                        dp[remaining] = (
-                            prev[remaining] +
-                            prev[remaining - value]
-                        ) % MOD
-
-        res = dp[halfSum]
-        print((res * pow(2, MOD - 2, MOD)) % MOD)  # Using modular inverse of 2
-    else:
-        print(0)
+    t = int(input())
+    while t > 0:
+        n, f, k = [int(v) for v in input().split()]
+        arr = [int(v) for v in input().split()]
+        favourite = arr[f - 1]
+        arr.sort(reverse=True)
+        value = arr[k - 1]
+        if value < favourite:
+            print("YES")
+        elif value > favourite:
+            print("NO")
+        else:
+            if k < n and arr[k] == favourite:
+                print("MAYBE")
+            else:
+                print("YES")
+        t -= 1
 
 
 main()
--- src/second.py
+++ src/second.py
@@ -1,1 +1,10 @@
-PI = 3.14
+def main():
+    t = int(input())
+    while t > 0:
+        n = int(input())
+        arr = [int(v) for v in input().split()]
+        print(arr)
+        t -= 1
+
+
+main()
