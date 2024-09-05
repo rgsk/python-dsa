fc5b15cd48de7565800d59e08905ad7fccf28af0
rgsk <guptarahul70322@gmail.com>
2024-09-05 10:34:47 +0530

    done

---
+++ src/CSES/1_Introductory Problems/19_Grid Paths/tle2.py
@@ -0,0 +1,51 @@
+import os
+import sys
+
+script_directory = os.path.dirname(os.path.realpath(__file__))
+if os.path.exists(os.path.join(script_directory, 'input.txt')):
+    sys.stdin = open(os.path.join(script_directory, 'input.txt'), 'r')
+    try:
+        sys.stdout = open(os.path.join(script_directory, 'output.txt'), 'w')
+    except Exception as e:
+        pass
+
+ways = 0
+visited = [[False for _ in range(7)] for _ in range(7)]
+path = ""
+
+
+def is_valid(i: int, j: int):
+    return i >= 0 and j >= 0 and i < 7 and j < 7 and (not visited[i][j])
+
+
+def dfs(i: int, j: int, steps: int):
+    global ways
+    if i == 6 and j == 0:
+        if steps == 48:
+            ways += 1
+        return
+
+    visited[i][j] = True
+    if (path[steps] == '?' or path[steps] == 'D') and is_valid(i + 1, j):
+        if not (not is_valid(i+2, j) and is_valid(i+1, j+1) and is_valid(i+1, j-1)):
+            dfs(i + 1, j, steps + 1)
+    if (path[steps] == '?' or path[steps] == 'U') and is_valid(i - 1, j):
+        if not (not is_valid(i-2, j) and is_valid(i-1, j+1) and is_valid(i-1, j-1)):
+            dfs(i - 1, j, steps + 1)
+    if (path[steps] == '?' or path[steps] == 'R') and is_valid(i, j + 1):
+        if not (not is_valid(i, j+2) and is_valid(i-1, j+1) and is_valid(i+1, j+1)):
+            dfs(i, j + 1, steps + 1)
+    if (path[steps] == '?' or path[steps] == 'L') and is_valid(i, j - 1):
+        if not (not is_valid(i, j-2) and is_valid(i-1, j-1) and is_valid(i+1, j-1)):
+            dfs(i, j - 1, steps + 1)
+    visited[i][j] = False
+
+
+def main():
+    global path
+    path = input()
+    dfs(0, 0, 0)
+    print(ways)
+
+
+main()
