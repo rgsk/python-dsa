a8591fa5aab896da171256e2fe6d734cfd9d78ea
rgsk <guptarahul70322@gmail.com>
2025-01-15 02:35:44 +0530

    triangle

--- src/math_concepts/magic.py
+++ src/math_concepts/magic.py
@@ -82,3 +82,13 @@ def cumulative_reverse(numbers):
         val += v
         result = [val] + result
     return result
+
+
+def triangle_area(x1, y1, x2, y2, x3, y3):
+    """
+    Returns the area of a triangle whose vertices are:
+    (x1, y1), (x2, y2), (x3, y3).
+    """
+    return abs(x1*(y2 - y3) +
+               x2*(y3 - y1) +
+               x3*(y1 - y2)) / 2.0
