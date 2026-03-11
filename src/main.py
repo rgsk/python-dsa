aa15249a84c5c5080d0ce4d2030bca28ac45a7f1
rgsk <guptarahul70322@gmail.com>
2026-03-11 23:29:21 +0530

    fix

--- src/main.py
+++ src/main.py
@@ -19,42 +19,14 @@ def setup(fn):
 
 @setup
 def main():
-    board = []
-    for _ in range(8):
-        board.append([v for v in input()])
-    diag_tr_to_bl = [False] * 15
-    diag_tl_to_br = [False] * 15
-    rows = [False] * 8
-    cols = [False] * 8
-
-    def update(r, c, val):
-        board[r][c] = 'Q' if val else '.'
-        rows[r] = val
-        cols[c] = val
-        diag_tr_to_bl[r + c] = val
-        diag_tl_to_br[r - c + 7] = val
-
-    def is_safe(r, c):
-        return not rows[r] and \
-            not cols[c] and \
-            not diag_tr_to_bl[r + c] and \
-            not diag_tl_to_br[r - c + 7]
-
-    count = 0
-
-    def helper(r):
-        nonlocal count
-        if r == 8:
-            count += 1
-            return
-        for c in range(8):
-            if is_safe(r, c) and board[r][c] != '*':
-                update(r, c, True)
-                helper(r + 1)
-                update(r, c, False)
-
-    helper(0)
-    print(count)
+    n = ii()
+    arr = li()
+    arr.sort()
+    distinct = 1
+    for i in range(1, n):
+        if arr[i] != arr[i-1]:
+            distinct += 1
+    print(distinct)
 
 
 if __name__ == "__main__":
