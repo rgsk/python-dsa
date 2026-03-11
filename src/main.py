7473c7baa218c472cbb6f75df347c2a7b16a2517
rgsk <guptarahul70322@gmail.com>
2026-03-11 23:23:01 +0530

    fix

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from collections import Counter
-from typing import List
+from typing import Any, List
 
 
 def ii():
@@ -19,30 +19,43 @@ def setup(fn):
 
 @setup
 def main():
-    arr = []
-    for _ in range(3):
-        arr.append(li())
-    clone = [row[:] for row in arr]
-    for i in range(3):
-        for j in range(3):
-            for di, dj in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
-                ni = i + di
-                nj = j + dj
-                if 0 <= ni < 3 and 0 <= nj < 3:
-                    clone[ni][nj] += arr[i][j]
+    board = []
+    for _ in range(8):
+        board.append([v for v in input()])
+    diag_tr_to_bl = [False] * 15
+    diag_tl_to_br = [False] * 15
+    rows = [False] * 8
+    cols = [False] * 8
 
-    for i in range(3):
-        for j in range(3):
-            if clone[i][j] % 2:
-                arr[i][j] = 0
-            else:
-                arr[i][j] = 1
-    for i in range(3):
-        for j in range(3):
-            print(arr[i][j], end='')
-        print()
+    def update(r, c, val):
+        board[r][c] = 'Q' if val else '.'
+        rows[r] = val
+        cols[c] = val
+        diag_tr_to_bl[r + c] = val
+        diag_tl_to_br[r - c + 7] = val
+
+    def is_safe(r, c):
+        return not rows[r] and \
+            not cols[c] and \
+            not diag_tr_to_bl[r + c] and \
+            not diag_tl_to_br[r - c + 7]
+
+    count = 0
+
+    def helper(r):
+        nonlocal count
+        if r == 8:
+            count += 1
+            return
+        for c in range(8):
+            if is_safe(r, c) and board[r][c] != '*':
+                update(r, c, True)
+                helper(r + 1)
+                update(r, c, False)
+
+    helper(0)
+    print(count)
 
 
 if __name__ == "__main__":
-    main()
     main()
